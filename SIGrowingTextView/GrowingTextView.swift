fd4f3d6cc6c0ccbb52d3234d799dc2b33c8124c4
TomoyaHayakawa <simorgh3196@gmail.com>
2016-05-24 16:25:50 +0900

    TextView周りの概形の作成

---
+++ SIGrowingTextView/GrowingTextView.swift
@@ -0,0 +1,9 @@
+//
+//  GrowingTextView.swift
+//  SIGrowingTextView
+//
+//  Created by 早川智也 on 2016/05/21.
+//  Copyright © 2016年 simorgh. All rights reserved.
+//
+
+import Foundation
---
+++ SIGrowingTextView/SIGrowingTextView.h
@@ -0,0 +1,19 @@
+//
+//  SIGrowingTextView.h
+//  SIGrowingTextView
+//
+//  Created by 早川智也 on 2016/05/21.
+//  Copyright © 2016年 simorgh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SIGrowingTextView.
+FOUNDATION_EXPORT double SIGrowingTextViewVersionNumber;
+
+//! Project version string for SIGrowingTextView.
+FOUNDATION_EXPORT const unsigned char SIGrowingTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SIGrowingTextView/PublicHeader.h>
+
+
